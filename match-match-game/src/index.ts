efe11f21fd3bcad83af31dcbef9e155c7526e0a9
Elena Zabalueva <age_eva@mail.ru>
2021-05-10 00:12:02 +0300

    init: start project with webpack

---
+++ match-match-game/src/index.ts
@@ -0,0 +1,7 @@
+/* console.log('Hello world'); */
+/* export type Fruit = 'Orange';
+const newJ: string = function grut(r: Fruit) : string {
+  return Array.from(r).reverse().toString();
+}; */
+
+console.log('newJ');
