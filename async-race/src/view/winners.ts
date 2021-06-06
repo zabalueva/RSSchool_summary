346c56eb9be3bcb1be547dba3a35f903907247dc
Elena Zabalueva <age_eva@mail.ru>
2021-06-06 18:15:07 +0300

    feat: add color and name for car

--- async-race/src/view/winners.ts
+++ async-race/src/view/winners.ts
@@ -1,30 +1,24 @@
 import { BaseComponent } from '../components/base/base';
 
 export class Winners extends BaseComponent {
-  private readonly about: HTMLDivElement;
+  private readonly winners: HTMLDivElement;
 
   constructor() {
-    super('div', ['about']);
-    this.about = document.createElement('div');
-    this.element.append(this.about);
+    super('div', ['winners']);
+    this.winners = document.createElement('div');
+    this.element.append(this.winners);
   }
 
   getView = {
     render: ():string => `
-    <div class="rules">
-    <p>Register! Game is available only for registered users.</p>
-    </div >
-    <div class="rules rules_step2">
-    <p>Configure your game settings</p>
-    </div >
-    <div class="rules rules_step3">
-    <p> Start your new game! Remember card positions and match it before times up </p>
+    <div class="page__winners">
+    Winners
     </div>
       `,
   };
 
   destroy(): void {
     this.element.innerHTML = '';
-    Array.from(document.querySelectorAll('.rules'))?.forEach((el) => el.classList.add('settings_hidden'));
+    Array.from(document.querySelectorAll('.page__winners'))?.forEach((el) => el.classList.add('hidden'));
   }
 }
