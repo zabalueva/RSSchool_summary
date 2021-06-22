9fc421ec6cf3a8dd1aed6e95d77d65d892db366c
Elena Zabalueva <age_eva@mail.ru>
2021-06-22 23:34:50 +0300

    feat: add store

---
+++ english-for-kids/my-app/src/app/store/states/category.ts
@@ -0,0 +1,28 @@
+import { Category } from "src/app/models/category";
+
+export const initialCategoriesState: Category[] = [
+  {
+    title: 'animal',
+  },
+  {
+    title: 'feat',
+  },
+  {
+    title: 'rtl',
+  },
+  {
+    title: 'emoji',
+  },
+  {
+    title: 'trees',
+  },
+  {
+    title: 'animal',
+  },
+  {
+    title: 'nature',
+  },
+  {
+    title: 'feel',
+  },
+];
