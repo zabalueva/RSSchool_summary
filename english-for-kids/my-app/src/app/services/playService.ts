6a9cf06a8c63a93f16e7b15dc4f0cb6596e7092e
Elena Zabalueva <age_eva@mail.ru>
2021-07-05 23:42:16 +0300

    feat: add correct remover

--- english-for-kids/my-app/src/app/services/playService.ts
+++ english-for-kids/my-app/src/app/services/playService.ts
@@ -2,42 +2,37 @@ import cards from "src/assets/cards";
 import { Card } from "../models/card";
 
 export class PlayService {
-  checkingWord = '';
-  cardsPlay: Card[] = [];
-  points: number = 0;
-  remover: Card[] = [];
+  checkingWord='';
+  cardsPlay: Card[]=[];
+  points: number=0;
+  remover: Card[]=[];
 
   getRandomSound(number: number) {
-    if (this.checkingWord === '') {
-console.log('dfkjdf');
-this.cardsPlay = cards[number].slice();
-this.remover = this.cardsPlay.slice();
+    if (this.checkingWord==='') {
+      this.cardsPlay=cards[number].slice();
+      this.remover=this.cardsPlay.slice();
     }
-
-
     let audio=new Audio();
-    const randomNumber = Math.floor(Math.random()*this.remover.length);
+    const randomNumber=Math.floor(Math.random()*this.remover.length);
     audio.src=this.remover[randomNumber].audioSrc;
     audio.load();
     audio.play();
-    this.checkingWord = this.remover[randomNumber].word;
+    this.checkingWord=this.remover[randomNumber].word;
     this.remover.splice(randomNumber, 1);
-    console.log(randomNumber);
-    console.log(this.cardsPlay);
   }
 
   repeatRandomSound(number: number) {
     let audio=new Audio();
-    cards[number].forEach((el) => el.word === this.checkingWord ? audio.src=el.audioSrc : '');
+    cards[number].forEach((el) => el.word===this.checkingWord? audio.src=el.audioSrc:'');
     console.log(audio.src)
     audio.load();
     audio.play();
   }
 
-  incrementPoints(){
-    this.points = this.points + 1;
+  incrementPoints() {
+    this.points=this.points+1;
   }
-  getPoints(){
+  getPoints() {
     return this.points;
   }
 }
