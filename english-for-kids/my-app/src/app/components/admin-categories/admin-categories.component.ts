91676d7a977e45cb1f014ea0b9772d85a64c4326
Elena Zabalueva <age_eva@mail.ru>
2021-07-08 23:05:52 +0300

    feat: add card category view

--- english-for-kids/my-app/src/app/components/admin-categories/admin-categories.component.ts
+++ english-for-kids/my-app/src/app/components/admin-categories/admin-categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import cards, { categories } from 'dist/my-app/assets/cards';
 
 @Component({
   selector: 'app-admin-categories',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-categories.component.scss']
 })
 export class AdminCategoriesComponent implements OnInit {
-
+public categories = categories;
+public cards = cards;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  deleteCategory(){
+    console.log('csr')
   }
 
 }
--- english-for-kids/my-app/src/app/components/admin/admin.component.ts
+++ english-for-kids/my-app/src/app/components/admin/admin.component.ts
@@ -10,6 +10,17 @@ export class AdminComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if (document.querySelector('.logout')){
+      document.querySelector('.login')?.classList.add('hidden');
+      document.querySelector('.toggle_button')?.classList.add('hidden')
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (document.querySelector('.logout')){
+      document.querySelector('.login')?.classList.remove('hidden');
+      document.querySelector('.toggle-button')?.classList.remove('hidden')
+    }
   }
 
 }
