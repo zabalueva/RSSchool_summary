9c6369e87e8e56ee415e20f8e0c38604172f1762
Elena Zabalueva <age_eva@mail.ru>
2021-07-08 22:19:43 +0300

    feat: add login form

--- english-for-kids/my-app/src/app/components/login/login.component.ts
+++ english-for-kids/my-app/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,77 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  public stopSubmit = false;
+  public stopSubmit=false;
 
-  clearForm(inputs:HTMLInputElement[]): void {
-    for (let j = 0; j < inputs.length; j++) {
-      inputs[j].value = '';
-    }
-    this.stopSubmit = false;
+  clearForm(): void {
+    const loginInput=document.querySelector('.form__input_login') as HTMLInputElement;
+    const passwordInput=document.querySelector('.form__input_password') as HTMLInputElement;
+    loginInput.value='';
+    passwordInput.value='';
   }
 
-  validateForm():void {
-    const nameInput = document.querySelector('.form__input_name') as HTMLInputElement;
-    const surnameInput = document.querySelector('.form__input_surname') as HTMLInputElement;
-    const emailInput = document.querySelector('.form__input_email') as HTMLInputElement;
-    const submitButton = document.querySelector('.form__submit') as HTMLButtonElement;
-    const cancelButton = document.querySelector('.form__cancel') as HTMLButtonElement;
-
-    const inputs: HTMLInputElement[] = [];
-    inputs.push(nameInput, surnameInput, emailInput);
-
-    /* if (submitButton) {
-      submitButton.addEventListener('click', (e) => {
-        for (let i = 0; i < inputs.length; i++) {
-          const input = inputs[i];
-          const inputCustomValidation = new CustomValidation();
-          if (inputCustomValidation.checkValidity(input).length) {
-            const customValidityMessageForHTML = inputCustomValidation.getInvaliditiesForHTML();
-            input.insertAdjacentHTML('afterend', `<p class="error-message">${customValidityMessageForHTML}</p>`);
-            this.stopSubmit = true;
-          }
-          if (cancelButton) {
-            cancelButton.addEventListener('click', (event) => {
-              event.preventDefault();
-              inputCustomValidation.clearInvalidities();
-              this.clearForm(inputs);
-            });
-          }
-        }
-
-        if (this.stopSubmit) {
-          submitButton.classList.add('btn_inactive');
-          e.preventDefault();
-        }
-
-        if (!this.stopSubmit) {
-          e.preventDefault();
-          this.element.classList.add('registrationForm_hidden');
-          submitButton.classList.remove('btn_inactive');
-          document.querySelector('.startButton')?.classList.remove('startButton_disabled');
-          this.dataBase?.openInitDB();
-          this.dataBase?.addUser(nameInput.value, surnameInput.value, emailInput.value);
-          this.clearForm(inputs);
-        }
-      });
+  checkLogin() {
+    const loginInput=document.querySelector('.form__input_login') as HTMLInputElement;
+    const passwordInput=document.querySelector('.form__input_password') as HTMLInputElement;
+    if (loginInput.value==='admin'&&passwordInput.value==='admin') {
+      this.router.navigate(['/admin']);
+    } else {
+      passwordInput.insertAdjacentHTML('afterend', `<p class="error-message">Введены неверные данные</p>`);
     }
-
-    if (cancelButton) {
-      cancelButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        this.clearForm(inputs);
-        const errors = document.querySelectorAll('.error-message');
-        if (errors) {
-          Array.from(document.querySelectorAll('.form__input'))?.forEach((el) => {
-            (el.nextSibling as Element).innerHTML = '';
-          });
-        }
-      });
-    } */
   }
 }
