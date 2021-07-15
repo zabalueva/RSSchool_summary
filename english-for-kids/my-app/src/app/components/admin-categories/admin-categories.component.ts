8e9829be12b1c2868ea6675801953b585b10d732
Elena Zabalueva <age_eva@mail.ru>
2021-07-15 22:03:47 +0300

    fix: change path to categories

--- english-for-kids/my-app/src/app/components/admin-categories/admin-categories.component.ts
+++ english-for-kids/my-app/src/app/components/admin-categories/admin-categories.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import cards, { categories } from 'dist/my-app/assets/cards';
+import cards, { categories } from 'src/assets/cards';
 import { ConfigService } from 'src/app/controllers/categoryControllers';
 
 @Component({
