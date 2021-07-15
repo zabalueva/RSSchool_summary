66c372d24024595761482cb9576b81f356e8ca71
Elena Zabalueva <age_eva@mail.ru>
2021-07-15 22:00:09 +0300

    feat: add http request

--- english-for-kids/my-app/src/app/components/admin-categories/admin-categories.component.ts
+++ english-for-kids/my-app/src/app/components/admin-categories/admin-categories.component.ts
@@ -1,27 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import cards, { categories } from 'dist/my-app/assets/cards';
+import { ConfigService } from 'src/app/controllers/categoryControllers';
 
 @Component({
   selector: 'app-admin-categories',
   templateUrl: './admin-categories.component.html',
-  styleUrls: ['./admin-categories.component.scss']
+  styleUrls: ['./admin-categories.component.scss'],
 })
-export class AdminCategoriesComponent implements OnInit {
-public categories = categories;
+export class AdminCategoriesComponent {
+public categories:any ;
 public cards = cards;
 public editMode = false;
-  constructor() { }
+public cat: any;
 
-  ngOnInit(): void {
-  }
+  constructor(public serviceRequest: ConfigService) {
+    this.categories = categories;
 
-  deleteCategory(){
-    console.log('delete')
   }
 
   createCategory(){
+    const newCardContainer = document.createElement('div');
+    newCardContainer.classList.add('card-container');
+    document.querySelector('.cards-container')?.appendChild(newCardContainer);
+    const newCard = document.createElement('div');
+    newCard.classList.add('category__card');
+    newCardContainer.appendChild(newCard);
     console.log('create')
+  }
+
+  sendCategory() {
+    const newCardContainer = document.createElement('div');
+    newCardContainer.classList.add('card-container');
+    document.querySelector('.cards-container')?.appendChild(newCardContainer);
+    const newCard = document.createElement('div');
+    newCard.classList.add('category__card');
+    newCardContainer.appendChild(newCard);
+    console.log('send')
   }
 
   enableEditMode(){
@@ -37,6 +52,13 @@ public editMode = false;
   updateCategory(){
     this.editMode = true;
     console.log('update')
+  }
+
+  deleteCategory(){
+    this.cat =  this.serviceRequest.getAllCategories();
+
+    console.log(this.cat);
+    console.log('del')
   }
 
 }
---
+++ english-for-kids/my-app/src/app/controllers/categoryControllers.ts
@@ -0,0 +1,96 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+
+
+public SERVER = 'http://localhost:3000';
+
+
+public getAllCategories = async () => {
+  const allCategoriesServer = this.http.get(`${this.SERVER}/categories`);
+  return allCategoriesServer;
+};
+
+
+public configUrl = 'assets/config.json';
+
+public getConfig = async () => {
+  return this.http.get(this.configUrl);
+}
+/*
+export const getTotalCount = async (page:number, limit = MAX_CARS_ON_PAGE): Promise<string|null> => {
+  const allCarsServer = await fetch(`${SERVER}/garage?_page=${page}&_limit=${limit}`);
+  const totalCount = allCarsServer.headers.get('X-Total-Count');
+  return totalCount;
+};
+
+export const createCar = async (body: CarRequest):Promise<Response> => {
+  const newCar = await fetch(`${SERVER}/garage`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return newCar;
+};
+
+export const updateCar = async (body: CarRequest, id: number):Promise<Response> => {
+  const selectedCar = await fetch(`${SERVER}/garage/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return selectedCar;
+};
+
+export const deleteCar = async (id: number): Promise<void> => {
+  await fetch(`${SERVER}/garage/${id}`, { method: 'DELETE' });
+};
+export const deleteWinner = async (id: number): Promise<void> => {
+  await fetch(`${SERVER}/winners/${id}`, { method: 'DELETE' });
+};
+
+export const getAllWinners = async (page:number, limit = MAX_CARS_ON_WIN_PAGE) => {
+  const allWinners = await fetch(`${SERVER}/winners?_page=${page}&_limit=${limit}`);
+  return allWinners.json();
+};
+
+export const getCountWinners = async (page:number, limit = MAX_CARS_ON_PAGE):Promise<string | null> => {
+  const allWinners = await fetch(`${SERVER}/winners?_page=${page}&_limit=${limit}`);
+  const totalCount = allWinners.headers.get('X-Total-Count');
+  return totalCount;
+};
+
+export const startEngine = async (id:number):Promise<{
+  velocity: number,
+  distance: number,
+}> => {
+  const startedCars = await fetch(`${SERVER}/engine/?id=${id}&status=started`);
+  return startedCars.json();
+};
+
+export const driveEngine = async (id:number):Promise<Response> => {
+  const drivedCars = await fetch(`${SERVER}/engine/?id=${id}&status=drive`);
+  return drivedCars;
+};
+
+export const createWinner = async (body: Winner):Promise<Response> => {
+  const newCar = await fetch(`${SERVER}/winners`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return newCar;
+}; */
+}
